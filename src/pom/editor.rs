284d2b5b1cee1c769e1be8d5a0613ded3051a4f9
Wyatt Herkamp <wherkamp@gmail.com>
2024-09-25 15:59:05 -0400

    More docs

--- src/pom/editor.rs
+++ src/pom/editor.rs
@@ -54,66 +54,151 @@ impl Default for PomEditor {
         editor
     }
 }
-macro_rules! top_level_getter_setter {
+macro_rules! simple_type_getter_setter {
     (
-        $set:ident, $get:ident, $name:literal
-    ) => {
-        #[doc = concat!("Sets the ", $name, " of the pom file")]
-        pub fn $set(&mut self, value: &str) {
-            let root = self.root();
-            let element = crate::editor::utils::get_or_create_top_level_element(
-                $name,
-                &mut self.document,
-                root,
-            );
-
-            element.set_text_content(&mut self.document, value);
+        $(#[$shared_docs:meta])*
+        $name:literal {
+            $(#[$set_docs:meta])*
+            set: $set:ident,
+            $(#[$get_docs:meta])*
+            get: $get:ident,
         }
-        #[doc = concat!("Gets the ", $name, " of the pom file")]
+    ) => {
+        $(#[$set_docs])*
+        $(#[$shared_docs])*
+        pub fn $set<S, O>(&mut self, value: O)
+            where
+                S: Into<String>,
+                O: Into<Option<S>>
+            {
+            let root = self.root();
+            let value: Option<S> = value.into();
+            if let Some(value) = value {
+                let value = value.into();
+                let element = crate::editor::utils::get_or_create_top_level_element(
+                    $name,
+                    &mut self.document,
+                    root,
+                );
+                element.set_text_content(&mut self.document, value);
+            }else{
+                let element = crate::editor::utils::find_element(root, $name, &self.document);
+                if let Some(element) = element {
+                    element.detach(&mut self.document).expect("Failed to remove element");
+                }
+            }
+        }
+        $(#[$get_docs])*
+        $(#[$shared_docs])*
         pub fn $get(&self) -> Option<String> {
             let root = self.root();
             let element = crate::editor::utils::find_element(root, $name, &self.document);
             return element.map(|x| x.text_content(&self.document));
         }
+
     };
     [
         $(
-            (
-                $set:ident, $get:ident, $name:literal
-            )
+            $(#[$shared_docs:meta])*
+            $name:literal {
+                $(#[$set_docs:meta])*
+                set: $set:ident,
+                $(#[$get_docs:meta])*
+                get: $get:ident,
+            }
         ),*
     ] => {
         $(
-            top_level_getter_setter!($set, $get, $name);
+
+            simple_type_getter_setter! {
+                $(#[$shared_docs])*
+                $name {
+                    $(#[$set_docs])*
+                    set: $set,
+                    $(#[$get_docs])*
+                    get: $get,
+                }
+            }
         )*
     };
 
 }
 macro_rules! top_level_structured_type {
     (
+        $(#[$set_docs:meta])*
         set: $set:ident,
+        $(#[$get_docs:meta])*
         get: $get:ident,
         $element_name:literal => $structured_type:ident,
     ) => {
-        #[doc = concat!("Gets the ", $element_name, " of the pom file")]
-        #[doc = "Returns None if the element does not exist"]
-        #[doc = "# Notes"]
-        #[doc = "Why doesn't this work like [BuildEditor] or [DependencyManagementEditor]?"]
-        #[doc = "This structure is very simple.  It is easier to just use a simple structure."]
+        $(#[$get_docs])*
         pub fn $get(&self) -> Result<Option<$structured_type>, XMLEditorError> {
             let root = self.root();
             find_element(root, $element_name, &self.document)
                 .map(|x| $structured_type::from_element(x, &self.document))
                 .transpose()
         }
-        /// Sets the parent of the pom file
-        pub fn $set(&mut self, value: $structured_type) -> Result<(), XMLEditorError> {
+        $(#[$set_docs])*
+        pub fn $set<U>(&mut self, value: U) -> Result<(), XMLEditorError>
+        where
+            U: Into<Option<$structured_type>> {
+            let value: Option<$structured_type> = value.into();
             let root = self.root();
-            if let Some(element) = find_element(root, $element_name, &self.document) {
-                value.update_element(element, &mut self.document)?;
+            let existing_element = find_element(root, $element_name, &self.document);
+            if let Some(value) = value{
+                if let Some(element) = existing_element {
+                    value.update_element(element, &mut self.document)?;
+                }
+                let new_element = value.into_element(&mut self.document)?;
+                root.push_child(&mut self.document, new_element.into())?;
+            }else{
+                if let Some(element) = existing_element {
+                    element.detach(&mut self.document)?;
+                }
             }
-            let new_element = value.into_element(&mut self.document)?;
-            root.push_child(&mut self.document, new_element.into())?;
+
+            Ok(())
+        }
+    };
+}
+
+macro_rules! list_item_getter_and_add {
+    (
+        $(#[$get_docs:meta])*
+        get: $get:ident,
+        $(#[$add_docs:meta])*
+        add: $add:ident,
+        $(#[$clear_docs:meta])*
+        clear: $clear:ident,
+        $parent:literal => $list_element:ident
+    ) => {
+        $(#[$get_docs])*
+        pub fn $get(&self) -> Result<Vec<$list_element>, XMLEditorError> {
+            let root = self.root();
+            let Some(parent_element) = find_element(root, $parent, &self.document)
+            else {
+                return Ok(vec![]);
+            };
+            let result =
+                get_all_children_of_element::<$list_element>(&self.document, parent_element)?;
+            Ok(result.into_iter().map(|(v, _)| v).collect())
+        }
+        $(#[$add_docs])*
+        pub fn $add(
+            &mut self,
+            value: $list_element,
+        ) -> Result<Option<$list_element>, XMLEditorError> {
+            let root = self.root();
+            let parent_element = find_element(root, $parent, &self.document);
+            add_or_update_item(&mut self.document, parent_element, root, value)
+        }
+        $(#[$clear_docs])*
+        pub fn  $clear(&mut self)-> Result<(), XMLEditorError> {
+            let root = self.root();
+            let parent_element = find_element(root, $parent, &self.document);
+            if let Some(parent_element) = parent_element {
+                parent_element.clear_children(&mut self.document);
+            }
             Ok(())
         }
     };
@@ -127,27 +212,232 @@ impl PomEditor {
         editor
     }
     top_level_structured_type!(
+        /// Sets the parent of the pom file
+        ///
+        /// If [None] is passed in. The parent element will be removed
         set: set_parent,
+        /// Gets the parent of the pom file
         get: get_parent,
         "parent" => Parent,
     );
     top_level_structured_type!(
+        /// Sets the scm of the pom file
+        ///
+        /// If [None] is passed in. The scm element will be removed
         set: set_scm,
+        /// Gets the scm of the pom file
         get: get_scm,
         "scm" => Scm,
     );
-    top_level_getter_setter![
-        (set_group_id, get_group_id, "groupId"),
-        (set_artifact_id, get_artifact_id, "artifactId"),
-        (set_version, get_version, "version"),
-        (set_name, get_name, "name"),
-        (set_description, get_description, "description"),
-        (get_model_version, set_model_version, "modelVersion"),
-        (set_url, get_url, "url"),
-        (set_packaging, get_packaging, "packaging")
+    simple_type_getter_setter![
+        /// The group id of the pom
+        ///
+        /// [More Info](https://maven.apache.org/pom.html#maven-coordinates)
+        /// Example Usage:
+        /// ```rust
+        /// use maven_rs::pom::editor::PomEditor;
+        /// let mut editor = PomEditor::default();
+        /// editor.set_group_id("dev.wyatt-herkamp");
+        /// assert_eq!(editor.get_group_id(), Some("dev.wyatt-herkamp".to_string()));
+        /// ```
+        "groupId" {
+            /// Sets the group Id in the pom file. For the maven project
+            set: set_group_id,
+            get: get_group_id,
+        },
+        /// The artifact id of the pom
+        /// [More Info](https://maven.apache.org/pom.html#maven-coordinates)
+        ///
+        /// Example Usage:
+        /// ```rust
+        /// use maven_rs::pom::editor::PomEditor;
+        /// let mut editor = PomEditor::default();
+        /// editor.set_artifact_id("test");
+        /// assert_eq!(editor.get_artifact_id(), Some("test".to_string()));
+        /// ```
+        "artifactId" {
+            set: set_artifact_id,
+            get: get_artifact_id,
+        },
+        /// The version of the project file
+        /// [More Info](https://maven.apache.org/pom.html#maven-coordinates)
+        ///
+        /// Example Usage:
+        /// ```rust
+        /// use maven_rs::pom::editor::PomEditor;
+        /// let mut editor = PomEditor::default();
+        /// editor.set_version("1.0.0");
+        /// assert_eq!(editor.get_version(), Some("1.0.0".to_string()));
+        /// ```
+        "version" {
+            set: set_version,
+            get: get_version,
+        },
+        /// The name of the maven project
+        ///
+        /// [More Info](https://maven.apache.org/pom.html#More_Project_Information)
+        "name" {
+            set: set_name,
+            get: get_name,
+        },
+        /// The description of the maven project
+        ///
+        /// [More Info](https://maven.apache.org/pom.html#More_Project_Information)
+        "description" {
+            set: set_description,
+            get: get_description,
+        },
+        /// [More Info](https://maven.apache.org/pom.html#More_Project_Information)
+        "url" {
+            set: set_url,
+            get: get_url,
+        },
+        /// The inception year of the project
+        "inceptionYear" {
+            set: set_inception_year,
+            get: get_inception_year,
+        },
+        /// Sets the model version of the pom file
+        ///
+        /// The model version is currently always 4.0.0
+        "modelVersion" {
+            /// Sets the model version of the pom file
+            set: set_model_version,
+            /// Gets the model version of the pom file
+            get: get_model_version,
+        },
+        /// [More Info](https://maven.apache.org/pom.html#Packaging)
+        "packaging" {
+            set: set_packaging,
+            get: get_packaging,
+        }
     ];
 
-    // TODO: Repositories, pluginRepositories
+    list_item_getter_and_add!(
+        /// Gets all the repositories in the pom file
+        /// ```rust
+        /// use maven_rs::pom::editor::PomEditor;
+        /// use maven_rs::pom::Repository;
+        /// let xml = r#"
+        /// <project>
+        ///   <repositories>
+        ///    <repository>
+        ///      <id>central</id>
+        ///      <name>Maven Central Repository</name>
+        ///      <url>https://repo.maven.apache.org/maven2</url>
+        ///   </repository>
+        /// </repositories>
+        /// </project>
+        /// "#;
+        /// let editor = PomEditor::load_from_str(xml).unwrap();
+        /// let repositories = editor.get_repositories().unwrap();
+        /// assert_eq!(repositories.len(), 1);
+        /// assert_eq!(repositories[0].id, Some("central".to_string()));
+        get: get_repositories,
+        /// Adds or Updates a repository in the pom file
+        /// ```rust
+        /// use maven_rs::pom::editor::PomEditor;
+        /// use maven_rs::pom::Repository;
+        /// let mut editor = PomEditor::default();
+        /// editor.add_or_update_repository(Repository {
+        ///   id: Some("central".to_string()),
+        ///   name: Some("Maven Central Repository".to_string()),
+        ///   url: "https://repo.maven.apache.org/maven2".to_string(),
+        ///   ..Default::default()
+        /// }).unwrap();
+        /// let repositories = editor.get_repositories().unwrap();
+        /// assert_eq!(repositories.len(), 1);
+        /// assert_eq!(repositories[0].id, Some("central".to_string()));
+        /// ```
+        add: add_or_update_repository,
+        /// Clears all the repositories in the pom file
+        clear: clear_repositories,
+        "repositories" => Repository
+    );
+    list_item_getter_and_add!(
+        /// Gets all the developers in the pom file
+        /// ```rust
+        /// use maven_rs::pom::editor::PomEditor;
+        /// use maven_rs::pom::Developer;
+        /// let xml = r#"
+        /// <project>
+        ///    <developers>
+        ///       <developer>
+        ///         <id>dev.wyatt-herkamp</id>
+        ///         <name>Wyatt Herkamp</name>
+        ///        </developer>
+        ///   </developers>
+        /// </project>
+        /// "#;
+        /// let editor = PomEditor::load_from_str(xml).unwrap();
+        /// let developers = editor.get_developers().unwrap();
+        /// assert_eq!(developers.len(), 1);
+        /// assert_eq!(developers[0].id, Some("dev.wyatt-herkamp".to_string()));
+        /// ```
+        get: get_developers,
+        /// Adds or Updates a developer in the pom file
+        /// ```rust
+        /// use maven_rs::pom::editor::PomEditor;
+        /// use maven_rs::pom::Developer;
+        /// let mut editor = PomEditor::default();
+        /// editor.add_or_update_developer(Developer {
+        ///    id: Some("dev.wyatt-herkamp".to_string()),
+        ///    name: Some("Wyatt Herkamp".to_string()),
+        ///    ..Default::default()
+        /// }).unwrap();
+        /// let developers = editor.get_developers().unwrap();
+        /// assert_eq!(developers.len(), 1);
+        /// assert_eq!(developers[0].id, Some("dev.wyatt-herkamp".to_string()));
+        /// ```
+        add: add_or_update_developer,
+        /// Clears all the developers in the pom file
+        clear: clear_developers,
+        "developers" => Developer
+    );
+    list_item_getter_and_add!(
+        /// Gets all the dependencies in the pom file
+        /// ```rust
+        /// use maven_rs::pom::editor::PomEditor;
+        /// use maven_rs::pom::Dependency;
+        /// let xml = r#"
+        /// <project>
+        ///  <dependencies>
+        ///   <dependency>
+        ///    <groupId>com.google.guava</groupId>
+        ///    <artifactId>guava</artifactId>
+        ///    <version>30.1-jre</version>
+        /// </dependency>
+        /// </dependencies>
+        /// </project>
+        /// "#;
+        /// let editor = PomEditor::load_from_str(xml).unwrap();
+        /// let dependencies = editor.get_dependencies().unwrap();
+        /// assert_eq!(dependencies.len(), 1);
+        /// assert_eq!(dependencies[0].group_id, "com.google.guava".to_string());
+        /// ```
+        get: get_dependencies,
+        /// Adds or Updates a dependency in the pom file
+        ///
+        /// ```rust
+        /// use maven_rs::pom::editor::PomEditor;
+        /// use maven_rs::pom::Dependency;
+        /// let mut editor = PomEditor::default();
+        /// editor.add_or_update_dependency(Dependency {
+        ///  group_id: "com.google.guava".to_string(),
+        /// artifact_id: "guava".to_string(),
+        /// version: Some("30.1-jre".parse().unwrap()),
+        /// ..Default::default()
+        /// }).unwrap();
+        /// let dependencies = editor.get_dependencies().unwrap();
+        /// assert_eq!(dependencies.len(), 1);
+        /// assert_eq!(dependencies[0].group_id, "com.google.guava".to_string());
+        /// ```
+        add: add_or_update_dependency,
+        /// Clears all the dependencies in the pom file
+        clear: clear_dependencies,
+        "dependencies" => Dependency
+    );
+    // TODO:  pluginRepositories
     // Loads a pom from a string
     pub fn load_from_str(value: &str) -> Result<Self, XMLEditorError> {
         let document = Document::parse_str_with_opts(
@@ -194,63 +484,6 @@ impl PomEditor {
             });
         }
         Ok(())
-    }
-    /// Gets all the repositories in the pom file
-    pub fn get_repositories(&self) -> Result<Vec<Repository>, XMLEditorError> {
-        let root = self.root();
-        let Some(repositories_element) = find_element(root, "repositories", &self.document) else {
-            return Ok(vec![]);
-        };
-        let result =
-            get_all_children_of_element::<Repository>(&self.document, repositories_element)?;
-        Ok(result.into_iter().map(|(repo, _)| repo).collect())
-    }
-    /// Adds or Updates a repository in the pom file
-    pub fn add_or_update_repository(
-        &mut self,
-        repository: Repository,
-    ) -> Result<Option<Repository>, XMLEditorError> {
-        let root = self.root();
-        let repositories_element = find_element(root, "repositories", &self.document);
-        add_or_update_item(&mut self.document, repositories_element, root, repository)
-    }
-
-    /// Gets all the repositories in the pom file
-    pub fn get_developers(&self) -> Result<Vec<Developer>, XMLEditorError> {
-        let root = self.root();
-        let Some(list_element) = find_element(root, "developers", &self.document) else {
-            return Ok(vec![]);
-        };
-        let result = get_all_children_of_element::<Developer>(&self.document, list_element)?;
-        Ok(result.into_iter().map(|(repo, _)| repo).collect())
-    }
-    /// Adds or Updates a repository in the pom file
-    pub fn add_or_update_developer(
-        &mut self,
-        developer: Developer,
-    ) -> Result<Option<Developer>, XMLEditorError> {
-        let root = self.root();
-        let list_element = find_element(root, "developers", &self.document);
-        add_or_update_item(&mut self.document, list_element, root, developer)
-    }
-    /// Gets all the dependencies in the pom file
-    pub fn get_dependencies(&self) -> Result<Vec<Dependency>, XMLEditorError> {
-        let root = self.root();
-        let Some(dependencies_element) = find_element(root, "dependencies", &self.document) else {
-            return Ok(vec![]);
-        };
-        let result =
-            get_all_children_of_element::<Dependency>(&self.document, dependencies_element)?;
-        Ok(result.into_iter().map(|(depend, _)| depend).collect())
-    }
-    /// Adds or Updates a dependency in the pom file
-    pub fn add_or_update_dependency(
-        &mut self,
-        dependency: Dependency,
-    ) -> Result<Option<Dependency>, XMLEditorError> {
-        let root = self.root();
-        let dependencies_element = find_element(root, "dependencies", &self.document);
-        add_or_update_item(&mut self.document, dependencies_element, root, dependency)
     }
 
     pub(crate) fn root(&self) -> Element {
