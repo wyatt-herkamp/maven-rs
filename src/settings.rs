714645e2c14a12bf3d1869b294a25759fb4388b6
Wyatt Herkamp <wherkamp@gmail.com>
2025-05-15 17:21:27 -0400

    Rust 1.87 undeprecated env::home_dir. So we can finally remove the dirs crate as a dependency!

--- src/settings.rs
+++ src/settings.rs
@@ -3,8 +3,11 @@ use std::path::PathBuf;
 use serde::{Deserialize, Serialize};
 mod mirrors;
 mod servers;
+use crate::Error;
 pub use mirrors::*;
 pub use servers::*;
+use std::env;
+use std::io::BufReader;
 
 pub static MAVEN_FOLDER: &str = ".m2";
 pub static SETTINGS_FILE: &str = "settings.xml";
@@ -23,59 +26,50 @@ impl Settings {
     pub fn get_local_repository(&self) -> Option<PathBuf> {
         self.local_repository.clone()
     }
-}
-#[cfg(feature = "local")]
-pub mod directories {
-    use super::{Settings, MAVEN_FOLDER, SETTINGS_FILE};
-    use crate::Error;
-    use std::io::BufReader;
-    use std::path::PathBuf;
-
-    /// Returns the path to the .m2 folder
-    ///
-    /// If the home directory is not found, None is returned.
-    ///
-    /// # Example
-    ///
-    /// ```
-    /// use maven_rs::settings::directories::get_settings_directory;
-    /// let path = get_settings_directory();
-    /// println!("{:?}", path);
-    /// ``````
-    pub fn get_settings_directory() -> Option<PathBuf> {
-        dirs::home_dir().map(|dirs| dirs.join(MAVEN_FOLDER))
-    }
-
-    /// Returns returns the path to the settings file.
-    pub fn get_settings_path() -> Option<PathBuf> {
-        get_settings_directory().map(|dir| dir.join(SETTINGS_FILE))
-    }
-
-    impl Settings {
-        /// Attempts to read the local configuration file.
-        pub fn read_local_config() -> Result<Settings, Error> {
-            let result = get_settings_path().ok_or(Error::NoHomeDirectory)?;
-            if !result.exists() {
-                return Ok(Settings::default());
-            }
-            let file = std::fs::File::open(result)?;
-            quick_xml::de::from_reader(BufReader::new(file)).map_err(Error::from)
+    /// Attempts to read the local configuration file.
+    pub fn read_local_config() -> Result<Settings, Error> {
+        let result = get_settings_path().ok_or(Error::NoHomeDirectory)?;
+        if !result.exists() {
+            return Ok(Settings::default());
         }
-        /// Returns the local repository or the default repository.
-        ///
-        /// If None is Returned Home Directory is not found.
-        pub fn get_local_repository_or_default(&self) -> Option<PathBuf> {
-            if let Some(local_repository) = &self.local_repository {
-                Some(local_repository.clone())
-            } else {
-                get_settings_directory().map(|dir| dir.join("repository"))
-            }
+        let file = std::fs::File::open(result)?;
+        quick_xml::de::from_reader(BufReader::new(file)).map_err(Error::from)
+    }
+    /// Returns the local repository or the default repository.
+    ///
+    /// If None is Returned Home Directory is not found.
+    pub fn get_local_repository_or_default(&self) -> Option<PathBuf> {
+        if let Some(local_repository) = &self.local_repository {
+            Some(local_repository.clone())
+        } else {
+            get_settings_directory().map(|dir| dir.join("repository"))
         }
     }
 }
+/// Returns the path to the .m2 folder
+///
+/// If the home directory is not found, None is returned.
+///
+/// # Example
+///
+/// ```
+/// use maven_rs::settings::directories::get_settings_directory;
+/// let path = get_settings_directory();
+/// println!("{:?}", path);
+/// ``````
+pub fn get_settings_directory() -> Option<PathBuf> {
+    env::home_dir().map(|dirs| dirs.join(MAVEN_FOLDER))
+}
 
-#[cfg(all(test, feature = "local"))]
+/// Returns returns the path to the settings file.
+pub fn get_settings_path() -> Option<PathBuf> {
+    get_settings_directory().map(|dir| dir.join(SETTINGS_FILE))
+}
+
+#[cfg(test)]
 pub mod tests {
+    use std::env;
+
     use crate::settings::{Server, Servers, Settings};
 
     #[test]
